772fbe5c8392a245dc8b577f7dbc9fc0275601bf
Matt March <matthew.march@ghyston.com>
2020-12-01 08:19:23 +0000

    Solve 1a

--- Day01.fs
+++ Day01.fs
@@ -1,9 +1,25 @@
 module Day01
 
-let input = "PuzzleInput"
+open Common
 
-let solveA input = "Solution to A"
+let input = readLines "Day01.txt" |> Seq.map int |> Seq.toList
+
+let targetValue = 2020
+
+let findSumPair inputList value =
+    if List.contains (targetValue - value) inputList
+    then Some (targetValue -  value, value)
+    else None
+
+let findPair input =
+    input |> List.pick (findSumPair input)
+
+let multiplyPair pair =
+    printfn "%A" pair
+    fst pair * snd pair
+
+let solveA input = findPair input |> multiplyPair
 
 let solveB input = "Solution to B"
 
-let solve input = Common.solveDay solveA solveB input
+let solve input = solveDay solveA solveB input
