070a62334b0f35c13b47a266c70ca9600d020dde
Matt March <matthew.march@ghyston.com>
2020-12-15 08:32:48 +0000

    Add back in original method used to solve part A

--- Day15.fs
+++ Day15.fs
@@ -37,3 +37,17 @@ let solveA = getNumberAtIndex (2020 - 1)
 let solveB = getNumberAtIndex (30000000 - 1)
 
 let solve: int list -> PartToSolve -> Unit = solveDay solveA solveB
+
+let memoryGameUnfolder previousValues =
+    let valueToFind = Seq.head previousValues
+    let valuesToSearch = Seq.tail previousValues
+    let nextValue = 
+        match Seq.tryFindIndex ((=) valueToFind) valuesToSearch with
+        | Some i -> i + 1
+        | None -> 0
+    Some (nextValue, Seq.append (Seq.singleton nextValue) previousValues)
+
+let memoryGame (startingNumbers: int seq) = 
+    Seq.append startingNumbers (Seq.unfold memoryGameUnfolder (Seq.rev startingNumbers))
+
+let originalSolveA = List.toSeq >> memoryGame >> Seq.take 2020 >> Seq.last
